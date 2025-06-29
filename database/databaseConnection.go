19db6525408291631ce61948df14291757e8abf7
rudraprasaaad <goudarudra23@gmail.com>
2025-06-29 17:34:14 +0530

    added database connection

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,48 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	MongoDb := os.Getenv("MONGODB_URL")
+
+	client, err := mongo.Connect(options.Client().ApplyURI(MongoDb))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	fmt.Println("Connected to Mongodb!! 🚀🚀")
+
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	return collection
+}
